cfe2f2f0881ff1a8fe02f43b5557b5cb7b6d2555
MelisaVba <melisabelenvillalba@gmail.com>
2026-06-15 07:33:18 -0300

    Implementacion de gestion de paises y persistencia CSV

--- paises.py
+++ paises.py
@@ -6,6 +6,148 @@ def cargar_datos():
     pass
 
 
+#! Guardar datos - CSV 
+
+def guardar_datos(nombre_archivo, paises):
+    try:
+        archivo = open(nombre_archivo, "w", encoding="utf-8")
+        archivo.write("nombre,poblacion,superficie,continente\n")
+        for pais in paises:
+            linea = (
+                pais["nombre"] + "," +
+                str(pais["poblacion"]) + "," +
+                str(pais["superficie"]) + "," +
+                pais["continente"] + "\n"
+            )
+
+            archivo.write(linea)
+        archivo.close()
+    except:
+        print("Error al guardar archivo")
+
+
+
+#! Cargar datos - CSV
+
+def cargar_datos(nombre_archivo):
+    paises = []
+    try:
+        archivo = open(nombre_archivo, "r", encoding="utf-8")
+        archivo.readline()
+        for linea in archivo:
+            datos = linea.strip().split(",")
+            if len(datos) != 4:
+                continue
+
+            pais = {
+                "nombre": datos[0],
+                "poblacion": int(datos[1]),
+                "superficie": int(datos[2]),
+                "continente": datos[3]
+            }
+            paises.append(pais)
+        archivo.close()
+
+    except FileNotFoundError:
+        print("Error: no se encontro el archivo")
+    except ValueError:
+        print("Error: formato incorrecto en el CSV")
+
+    return paises
+
+
+
+#! Buscar PAIS
+
+def buscar_pais(paises, nombre):
+    encontrado = False
+    nombre = nombre.lower().strip()
+    for pais in paises:
+        if nombre in pais["nombre"].lower():
+
+            print(
+                pais["nombre"],
+                "| Poblacion:", pais["poblacion"],
+                "| Superficie:", pais["superficie"],
+                "| Continente:", pais["continente"]
+            )
+
+            encontrado = True
+    if not encontrado:
+        print("No se encontraron coincidencias")
+
+
+
+#! Agregar PAIS
+
+def agregar_pais(paises):
+    nombre = input("Nombre: ").strip()
+    if nombre == "":
+        print("Nombre no valido")
+        return
+    
+    for pais in paises:
+        if pais["nombre"].lower() == nombre.lower():
+            print("El pais ya existe")
+            return
+    try:
+        poblacion = int(input("Poblacion: "))
+        superficie = int(input("Superficie: "))
+        if poblacion < 0 or superficie < 0:
+            print("Los valores no pueden ser negativos")
+            return
+        
+    except ValueError:
+        print("Debe ingresar numeros")
+        return
+
+    continente = input("Continente: ").strip()
+    if continente == "":
+        print("Continente no valido")
+        return
+
+    nuevo_pais = {
+        "nombre": nombre,
+        "poblacion": poblacion,
+        "superficie": superficie,
+        "continente": continente
+    }
+
+    paises.append(nuevo_pais)
+    guardar_datos("paises.csv", paises)
+    print("Pais agregado correctamente")
+
+
+
+#! Actualuzar PAIS
+
+def actualizar_pais(paises):
+    nombre = input("Pais a actualizar: ").strip()
+    for pais in paises:
+        if pais["nombre"].lower() == nombre.lower():
+            
+            try:
+                poblacion = int(input("Nueva poblacion: "))
+                superficie = int(input("Nueva superficie: "))
+                if poblacion < 0 or superficie < 0:
+                    print("Los valores no pueden ser negativos")
+                    return
+
+                pais["poblacion"] = poblacion
+                pais["superficie"] = superficie
+                guardar_datos("paises.csv", paises)
+                print("Datos actualizados")
+
+            except ValueError:
+                print("Debe ingresar numeros")
+
+            return
+    print("Pais no encontrado")
+
+
+
+
+
 #º  MENU
 
 def mostrar_menu():
